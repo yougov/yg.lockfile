2a1ab603b582697ac2ae81e1a61ffa67d542c579
Jason R. Coombs <jaraco@jaraco.com>
2014-02-28 16:54:34 -0500

    Here's an implementation of an ExecutionContext for locking an already-open file.

--- yg/lockfile/__init__.py
+++ yg/lockfile/__init__.py
@@ -25,7 +25,34 @@ logging.getLogger('zc.lockfile').setLevel(logging.ERROR+1)
 class FileLockTimeout(Exception):
     pass
 
-class FileLock(object):
+class LockBase(object):
+    def __init__(self, timeout=10, delay=.05):
+        if isinstance(timeout, numbers.Number):
+            timeout = datetime.timedelta(seconds=timeout)
+        if isinstance(delay, numbers.Number):
+            delay = datetime.timedelta(seconds=delay)
+        self.timeout = timeout
+        self.delay = delay
+
+    def __enter__(self):
+        """
+        Acquire the lock unless already locked.
+        """
+        if not self.is_locked():
+            self.acquire()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.release()
+
+    def __del__(self):
+        """
+        Release the lock on destruction.
+        """
+        self.release()
+
+
+class FileLock(LockBase):
     """
     A cross-platform locking file context.
 
@@ -37,21 +64,17 @@ class FileLock(object):
     This class is not threadsafe.
     """
 
-    def __init__(self, lockfile, timeout=10, delay=.05):
+    def __init__(self, lockfile, *args, **kwargs):
         """
-        Construct a FileLock. Specify the file to lock and optionally
-        the maximum timeout and the delay between each attempt to lock.
+        Construct a FileLock. Specify the path to the file to lock and
+        optionally the maximum timeout and the delay between each attempt to
+        lock.
 
         Timeout and delay can be given in numeric seconds or as
         `datetime.timedelta` objects.
         """
         self.lockfile = lockfile
-        if isinstance(timeout, numbers.Number):
-            timeout = datetime.timedelta(seconds=timeout)
-        if isinstance(delay, numbers.Number):
-            delay = datetime.timedelta(seconds=delay)
-        self.timeout = timeout
-        self.delay = delay
+        super(FileLock, self).__init__(*args, **kwargs)
 
     def acquire(self):
         """
@@ -87,19 +110,47 @@ class FileLock(object):
             del self.lock
             os.remove(self.lockfile)
 
-    def __enter__(self):
-        """
-        Acquire the lock unless we already have it.
-        """
-        if not self.is_locked():
-            self.acquire()
-        return self
 
-    def __exit__(self, type, value, traceback):
-        self.release()
+class ExclusiveContext(LockBase):
+    """
+    An exclusive context on an existing, open file.
+    """
+    def __init__(self, file, *args, **kwargs):
+        """
+        file should be an open file
+        """
+        self.file = file
+        super(ExclusiveContext, self).__init__(*args, **kwargs)
 
-    def __del__(self):
+    def acquire(self):
         """
-        Release the lock on destruction.
+        Attempt to acquire the lock every `delay` seconds until the
+        lock is acquired or until `timeout` has expired.
+
+        Raises FileLockTimeout if the timeout is exceeded.
+
+        Errors opening the lock file (other than if it exists) are
+        passed through.
         """
-        self.release()
+        stopwatch = timing.Stopwatch()
+        attempt = functools.partial(zc.lockfile._lock_file, self.file)
+        while True:
+            try:
+                self.lock = attempt()
+                break
+            except zc.lockfile.LockError:
+                timeout_expired = stopwatch.split() >= self.timeout
+                if timeout_expired:
+                    raise FileLockTimeout()
+                time.sleep(total_seconds(self.delay))
+
+    def is_locked(self):
+        return hasattr(self, 'lock')
+
+    def release(self):
+        """
+        Release the lock and delete the lockfile.
+        """
+        if self.is_locked():
+            zc.lockfile._unlock_file(self.file)
+            del self.lock
